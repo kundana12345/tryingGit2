fbb0695fc832f439f26519139d3b9bf212e5e323
Kundana Addala <kundanaaddala@gmail.com>
2023-08-03 18:04:59 -0500

    Initial Commit

---
+++ tryingGit2/tryingGit2App.swift
@@ -0,0 +1,17 @@
+//
+//  tryingGit2App.swift
+//  tryingGit2
+//
+//  Created by Kundana Addala on 8/3/23.
+//
+
+import SwiftUI
+
+@main
+struct tryingGit2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
